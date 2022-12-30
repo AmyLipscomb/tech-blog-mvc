8b0535b39512a84d70f7dfbfd3b7894b7b23fb91
AmyLipscomb <amy.a.lipscomb@gmail.com>
2022-12-30 08:18:57 -0500

    added code to 'user' model

--- models/User.js
+++ models/User.js
@@ -12,25 +12,51 @@ class User extends Model {
 User.init(
 {
     //id:
-    
-
-
-
-
-
-
-
-
-
-
-
-
+    id: {
+        type: DataTypes.INTEGER, // * Integer.
+        allowNull: false,  // * Doesn't allow null values.
+        primaryKey: true, // * Set as primary key.
+        autoIncrement: true,  // * Uses auto increment.
+    },
+    //name:
+    name: {
+        type: DataTypes.STRING, // * String
+        allowNull: false,
+    },
+    //email: 
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    //username
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    //password:
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+    },
+    {   
+        hooks: {
+          beforeCreate: async (newOwnerData) => {
+            newOwnerData.password = await bcrypt.hash(newOwnerData.password, 10);
+            return newOwnerData;
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
 }
 
+);
 
-
-
-
-
-
-)
+module.exports = User;
