8a5da6e48f170eb73c8f7134ff52edc98fdb6eff
AmyLipscomb <amy.a.lipscomb@gmail.com>
2022-12-30 08:23:30 -0500

    added some coding to 'index' model

---
+++ models/Index.js
@@ -0,0 +1,18 @@
+//import models
+const Blog = require('./Blog');
+const User = require('./User');
+
+// A user can have many (hasMany) blog posts
+
+
+
+// A blog post can only have (belogsTo) one user 
+
+
+
+// Add module.exports
+
+
+
+
+
