f63910521564e0128234782e9861e9cff446db57
AmyLipscomb <amy.a.lipscomb@gmail.com>
2022-12-30 08:30:18 -0500

    updated coding in 'index' model

--- models/Blog.js
+++ models/Blog.js
@@ -1,6 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        //id
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
 
 
 
+
+
+
+
+
+        //user_id
+        user_id: {
+            type: DataTypes.INTEGER,
+            // * References the `User` model's `id`.
+            references: {
+            model: "user",
+            key: "id",
+            },
+      },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "blog",
+      }
+);
+
+module.exports = Blog;
 
 
 
--- models/Index.js
+++ models/Index.js
@@ -3,16 +3,22 @@ const Blog = require('./Blog');
 const User = require('./User');
 
 // A user can have many (hasMany) blog posts
-
-
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
 
 // A blog post can only have (belogsTo) one user 
-
-
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
 
 // Add module.exports
-
-
+module.exports = {
+    Blog,
+    User,
+};
 
 
 
