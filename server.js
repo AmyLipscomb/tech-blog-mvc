361adef0942512797b822ab42798dd643f3acc41
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-01-11 08:09:54 -0500

    added commenting and notes to the server.js file

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+// Dependencies
 require('dotenv').config();
-const path = require ('path');
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
+const path = require ('path'); 
+const express = require('express'); 
+const routes = require('./controllers'); 
+const sequelize = require('./config/connection'); 
+const exphbs = require('express-handlebars'); 
 const session = require('express-session')
+const hbs = exphbs.create({}); 
 
-
-
-const hbs = exphbs.create({});
+//Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 6001;
 
 const { Blog, User } = require('./models'); 
+
+//Set Handlebars as the default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -31,7 +33,7 @@ app.use(session({
 
 app.use(routes);
 
-
+// Starts the server to begin listening
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
 });
